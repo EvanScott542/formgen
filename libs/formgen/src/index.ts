b4cc092c17590aae3748a97bf859a702cf3626d8
Evan Scott <evan.mark.scott@gmail.com>
2026-02-21 15:05:33 -0500

    feat: scaffold formgen ui library

---
+++ libs/formgen/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/formgen-ui/formgen-ui';
---
+++ libs/formgen/src/lib/formgen-ui/formgen-ui.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-formgen-ui',
+  imports: [],
+  templateUrl: './formgen-ui.html',
+  styleUrl: './formgen-ui.scss',
+})
+export class FormgenUi {}
